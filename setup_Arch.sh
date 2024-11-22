a8386c2a1e4849feb4719b3035194989dab85deb
rei_512_ <me@deltav-lab.org>
2024-11-22 12:59:51 +0900

    [waybar]add battery status, [Code]add, [setup_Arch]change

--- setup_Arch.sh
+++ setup_Arch.sh
@@ -10,16 +10,22 @@ DEST_ROOT="/"
 # ~/.config にリンクを作成
 if [ -d "$SRC_HOME" ]; then
     mkdir -p "$DEST_HOME"
-    for file in $(find "$SRC_HOME" -type f); do
+    find "$SRC_HOME" -type f -print0 | while IFS= read -r -d '' file; do
         relative_path="${file#$SRC_HOME/}"
         dest_path="$DEST_HOME/$relative_path"
+        
+        # リンクが既に存在する場合はスキップ
+        if [ -L "$dest_path" ]; then
+            continue
+        fi
         
         mkdir -p "$(dirname "$dest_path")"
         
         ln -sf "$file" "$dest_path"
         
-        # リンクが有効かどうかを確認
-        if [ ! -e "$dest_path" ]; then
+        if [ -e "$dest_path" ]; then
+            echo "リンクを作成しました: $dest_path -> $file"
+        else
             echo "リンクに失敗しました: $dest_path"
         fi
     done
@@ -27,18 +33,24 @@ else
     echo "$SRC_HOME が見つかりません。"
 fi
 
-# /usr/ROOT にリンクを作成
+# / にリンクを作成
 if [ -d "$SRC_ROOT" ]; then
-    for file in $(find "$SRC_ROOT" -type f); do
+    find "$SRC_ROOT" -type f -print0 | while IFS= read -r -d '' file; do
         relative_path="${file#$SRC_ROOT/}"
         dest_path="$DEST_ROOT/$relative_path"
+        
+        # リンクが既に存在する場合はスキップ
+        if [ -L "$dest_path" ]; then
+            continue
+        fi
         
         sudo mkdir -p "$(dirname "$dest_path")"
         
         sudo ln -sf "$file" "$dest_path"
         
-        # リンクが有効かどうかを確認
-        if [ ! -e "$dest_path" ]; then
+        if [ -e "$dest_path" ]; then
+            echo "リンクを作成しました: $dest_path -> $file"
+        else
             echo "リンクに失敗しました: $dest_path"
         fi
     done
