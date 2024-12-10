bc4fe290c222c164d4654c94fa1ef085ebc28266
rei_512_ <me@deltav-lab.org>
2024-12-10 17:41:39 +0900

    いっぱい

--- dotfiles/HOME/.bashrc
+++ dotfiles/HOME/.bashrc
@@ -1,2 +1,4 @@
 fish
 
+
+export STM32_PRG_PATH=/home/user/st/STM32CubeProgrammer/bin
--- dotfiles/HOME/.config/fish/config.fish
+++ dotfiles/HOME/.config/fish/config.fish
@@ -1,4 +1,4 @@
 alias ls 'lsd -a'
 export LC_ALL=en_US.utf8
 export PICO_SDK_PATH=/home/user/src/pico/pico-sdk
-export PICO_SDK_PATH=/home/user/pico-sdk
+
